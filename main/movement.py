bea91bc50e9869bef14aaa545b3194fc17dfbe84
suyeon1109 <97889490+suyeon1109@users.noreply.github.com>
2022-03-20 10:02:27 +0900

    Update movement.py

--- main/movement.py
+++ main/movement.py
@@ -4,7 +4,7 @@ pygame.init() #시작하겠습니다
 
 #window size of 500 width, 500 height
 win = pygame.display.set_mode((500,500))
-win = pygame.display.set_caption("Movement Practice")
+pygame.display.set_caption("Movement Practice")
 
 #캐릭터의 위치와 트기
 x = 50
@@ -19,7 +19,22 @@ while run:
     for event in pygame.event.get(): #어떤 이벤트가 일어났을때 모든 정보를 수집해라
         if event.type == pygame.QUIT:
             run = False 
+    keys = pygame.key.get_pressed()
+    print(keys)
 
+    if keys[pygame.K_LEFT]:
+        x -= velocity
+
+    if keys[pygame.K_RIGHT]:
+        x += velocity
+
+    if keys[pygame.K_UP]:
+        y -= velocity
+
+    if keys[pygame.K_DOWN]:
+        y += velocity
+
+    win.fill((0,0,0))
     pygame.draw.rect(win, (255,0,0), (x,y,width,height))
     pygame.display.update() #rectangle을 볼수 있게 해줌 frame(깜빡이는 속도)
 pygame.quit()
